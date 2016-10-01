89c610130f3bc99e09bc6e8fe73245ac802eb763
bernard peh <bpeh@sitecritic.net>
2016-10-02 00:05:10 +1000

    update symfony

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -7,7 +7,7 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -25,10 +25,10 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
 	        new Bpeh\NestablePageBundle\BpehNestablePageBundle(),
             new AppBundle\Page(),
-	        new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
-        );
+	        new Ivory\CKEditorBundle\IvoryCKEditorBundle()
+        ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -37,6 +37,21 @@ class AppKernel extends Kernel
         }
 
         return $bundles;
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
