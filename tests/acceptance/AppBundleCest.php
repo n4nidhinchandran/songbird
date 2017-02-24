d8a4079fa06d532384f7a84f65d2031313e2d8c6
Nidhin Chandran <nchandran@presidency.gov.rw>
2017-02-24 18:06:01 +0200

    test modified to check home page “not exists”

--- tests/acceptance/AppBundleCest.php
+++ tests/acceptance/AppBundleCest.php
@@ -12,10 +12,26 @@ class AppBundleCest
     }
 
     // tests
+    /*
+     * check homepage is active
+     */
+     /*
     public function InstallationTest(AcceptanceTester $I)
     {
         $I->wantTo('Check if Symfony is installed');
         $I->amOnPage('http://songbird.app/');
         $I->see('Symfony 3.1.4');
+    }
+    */
+    /**
+     * check homepage is not active
+     *
+     * @param AcceptanceTester $I
+     */
+    public function RemovalTest(AcceptanceTester $I)
+    {
+        $I->wantTo('Check if homepage is not active');
+        $I->amOnPage('http://songbird.app/');
+        $I->see('404 Not Found');
     }
 }
